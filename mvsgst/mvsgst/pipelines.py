872d2c2f7adbf8a61f3fe376be23eebbd8a4d302
miemiekurisu <miemiekurisu@aol.com>
2015-03-07 19:54:44 +0800

    update

--- mvsgst/mvsgst/pipelines.py
+++ mvsgst/mvsgst/pipelines.py
@@ -4,8 +4,15 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
 
 
 class MvsgstPipeline(object):
+    def __init__(self):
+         self.mfile = open('data.json','a')
+
     def process_item(self, item, spider):
+        
         return item
+    def close_spider(self,spider):
+         self.mfile.close()
--- mvsgst/mvsgst/spiders/vcmv.py
+++ mvsgst/mvsgst/spiders/vcmv.py
@@ -9,46 +9,44 @@ import time
 import re
 
 class DbmvSpider(scrapy.Spider):
-    name = "dbmv"
+    name = "vcmv"
     tags = []
-    for i in range(1900,2016,1):
-        tags.append( "http://movie.douban.com/tag/"+str(i))
-    start_urls = ("http://movie.douban.com/tag/1901/",) #tuple(tags)
+#    for i in range(1900,2016,1):
+#        tags.append( "http://movie.douban.com/tag/"+str(i))
+    start_urls = ("http://www.verycd.com/base/movie/~all/",) #tuple(tags)
 
     def load_item(self,ct):
+        staticurl = "http://www.verycd.com"
         item = MvsgstItem()
-        item['url']=ct.xpath('@href').extract()
+        item['url']=[]
+        for i in ct.xpath('@href').extract():
+                item['url'].append(staticurl+i)
         return item
 
     def load_detail(self,itemurl):
-        pre1 =re.compile(ur'\/\u5730\u533a\:.*br\/>')
-        pre2 = re.compile(ur'\u8bed\u8a00\:.*\<b')
-        pre3 = re.compile(ur'\>(.*)\<')
- 
+        dre = re.compile(ur'\u4e0a\u6620\u65e5\u671f.*\>(.*)\<\/em\>')
         details = itemurl.meta['item']         
         x = scrapy.Selector(itemurl)
-        details['mvname']= x.xpath('//*[@property="v:itemreviewed"]/text()').extract()
+        details['mvname']= x.xpath('//*[@class="titleDiv"]/h1/text()').extract()
+        details['enname']=x.xpath('//*[@class="titleDiv"]/h2/text()').extract()
         details['director']=x.xpath('//*[@rel="v:directedBy"]/text()').extract()
         details['actors']=x.xpath('//*[@rel="v:starring"]/text()').extract()
-        details['types']=x.xpath('//*[@property="v:genre"]/text()').extract()
-        details['date'] = x.xpath('//*[@property="v:initialReleaseDate"]/text()').extract()
+        details['types']=x.xpath('//*[@rel="v:genre"]/text()').extract()
+        details['date'] = dre.findall(itemurl.body.decode('utf8'))
         details['length'] = x.xpath('//*[@property="v:runtime"]/text()').extract()
         details['summary'] = x.xpath('//*[@property="v:summary"]/text()').extract()
+        #details['rank']= x.xpath('//*[@id="scoreDivDiv"]/text()').extract()
         #TODO this place should be rebuilt, but for this case, enough
-        type(itemurl.body)
-        details['areas'] = pre3.findall(pre1.findall(itemurl.body.decode('utf8')))
-        details['language']=pre3.findall(pre2.findall(itemurl.body.decode('utf8')))
         return details
 
     def parse(self, response):
 
         x = scrapy.Selector(response)
-        sites = x.xpath('//*[@id="content"]/div/div/div/table/tr/td/a')
+        sites = x.xpath('//*[@class="clearfix entry_cover_list"]/li/a')
         for ct in sites:
             item = self.load_item(ct)
             yield scrapy.Request(item['url'][0],meta={'item':item},callback=self.load_detail)
-
-        nexturl=x.xpath('//*[@class="next"]/a/@href').extract()
-        if len(nexturl)!=0:
-            print str(nexturl[0])
-            yield scrapy.Request(nexturl[0],callback=self.parse)  
+        time.sleep(8)
+        nexturl=x.xpath('//*[@rel="next"]/@href').extract()
+        if len(nexturl)>0:
+            yield scrapy.Request('http://www.verycd.com'+nexturl[0],callback=self.parse)  
